e24e07173c185c1f271dcf441155e15342fb6e1c
suman32665 <suman.maharjan@deerwalk.edu.np>
2015-01-19 09:46:04 -0800

    animal pattern

---
+++ Animalpattern/src/Animalpattern.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Suman-pc on 1/19/2015.
+ */
+public class Animalpattern {
+    public static void main(String[] args){
+
+        System.out.println("            ' _ ' ");
+        System.out.println("            (\u00A9\u00A9)");
+        System.out.println("  / ======== \\/ ");
+        System.out.println(" /  || %% ||");
+        System.out.println("*   ||----||");
+        System.out.println("    \u00A5\u00A5    \u00A5\u00A5");
+        System.out.println("    \"\"    \"\"");
+    }
+
+}
